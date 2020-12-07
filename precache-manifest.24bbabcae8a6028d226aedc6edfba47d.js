a15dd99f9fb02eb936d49f023d2280226b6509c7
siddharthtejas <siddharth243578@gmail.com>
2020-12-07 09:08:46 +0530

    Updates

---
+++ precache-manifest.24bbabcae8a6028d226aedc6edfba47d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b58852fab773c28c1f224a995a9b56c6",
+    "url": "/initialproject/index.html"
+  },
+  {
+    "revision": "dfe278a9f1cfc885e152",
+    "url": "/initialproject/static/css/main.856afaad.chunk.css"
+  },
+  {
+    "revision": "12c76ddfb7c2e14ecbae",
+    "url": "/initialproject/static/js/2.bf3d5c98.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/initialproject/static/js/2.bf3d5c98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfe278a9f1cfc885e152",
+    "url": "/initialproject/static/js/main.ca3d6435.chunk.js"
+  },
+  {
+    "revision": "fd4e355a1db858d14b81",
+    "url": "/initialproject/static/js/runtime-main.a257b486.js"
+  },
+  {
+    "revision": "f51001864b8af985baee8ba1122cd04d",
+    "url": "/initialproject/static/media/image-1.f5100186.png"
+  }
+]);
